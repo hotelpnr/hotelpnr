79b87e1e66a2419bb3dc0caac4420a2031e1c776
Your Name <you@example.com>
2023-07-19 10:53:23 +0530

    email fixed

--- Readme.txt
+++
@@ -1,6 +0,0 @@
-Thanks for downloading this template!
-
-Template Name: hotel PNR
-Template URL: https://BridgeWisely.com/hotel PNR-bootstrap-construction-website-template/
-Author: BridgeWisely.com
-License: https://BridgeWisely.com/license/
--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -3,7 +3,71 @@
 * URL: https://bootstrapmade.com/php-email-form/
 * Author: BootstrapMade.com
 */
+
+//let serviceID = 'hotelPNR_k07767d';
+//let templateID = 'template_brgz8ux';
+
 (function () {
+  "use strict";
+
+  let forms = document.querySelectorAll('.php-email-form');
+
+  forms.forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      let thisForm = this;
+
+      let serviceID = 'hotelPNR_k07767d';
+      let templateID = 'template_brgz8ux';
+
+      if (!serviceID || !templateID) {
+        displayError(thisForm, 'EmailJS configuration is incomplete!');
+        return;
+      }
+
+      thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+      let formData = new FormData(thisForm);
+
+      emailjs_submit(thisForm, serviceID, templateID, formData);
+    });
+  });
+
+  function emailjs_submit(thisForm, serviceID, templateID, formData) {
+    emailjs.send(serviceID, templateID, {
+      "name": formData.get("name"),
+      "email": formData.get("email"),
+      "phone": formData.get("phone"),
+      "message": formData.get("message"),
+    })
+      .then(function (response) {
+        thisForm.querySelector('.loading').classList.remove('d-block');
+        if (response.status === 200) {
+          thisForm.querySelector('.sent-message').classList.add('d-block');
+          thisForm.reset();
+        } else {
+          throw new Error('EmailJS API error: ' + response.text);
+        }
+      })
+      .catch(function (error) {
+        displayError(thisForm, error);
+      });
+  }
+
+  function displayError(thisForm, error) {
+    thisForm.querySelector('.loading').classList.remove('d-block');
+    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').classList.add('d-block');
+  }
+
+})();
+
+
+
+/*(function () {
   "use strict";
 
   let forms = document.querySelectorAll('.php-email-form');
@@ -82,4 +146,4 @@
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
-})();
+})();*/
